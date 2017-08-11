d0b6f57564390ba85b5ecc5ac69dad585a96a501
PraDeep <pradeepkotian1995@gmail.com>
2017-08-11 22:36:44 +0530

    Add files via upload

--- bootcamp/settings.py
+++ bootcamp/settings.py
@@ -31,9 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'notice',
-    'pages',
-    'form',      
+    'notice',         
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
